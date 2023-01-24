d27744d48ffcd6429281bf6bd9deda43a1822bda
arturSTPD <khl.artur@gmail.com>
2023-01-24 21:51:46 +0200

    Add first receiving-answering system

--- Controllers/BotController.cs
+++ Controllers/BotController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
-
+using MoodBot.Services;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
 
 namespace MoodBot.Controllers
 {
@@ -9,20 +11,17 @@ namespace MoodBot.Controllers
     public class BotController : ControllerBase
     {
         private readonly ILogger<BotController> _logger;
-        TelegramBotClient client { get; }
 
         public BotController(ILogger<BotController> logger)
         {
             _logger = logger;
-            var token = Environment.GetEnvironmentVariable("TelegramToken");
-            client = new TelegramBotClient(token);
         }
 
         [HttpGet("CheckBot")]
         public async Task<string> CheckBot()
         {
-            var me = await client.GetMeAsync();
-
+            BotService bot = new BotService();
+            var me = await bot.GetBotClient().GetMeAsync();
             return $"Hello, World! I am user {me.Id} and my name is {me.FirstName}.";
         }
     }
